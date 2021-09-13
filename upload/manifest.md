ceae47a484f54675b6d35c96836f3dc692f287d8
Lauren <lauren.tilton@gmail.com>
2021-09-13 10:31:11 -0400

    Add files via upload

---
+++ upload/manifest.md
@@ -0,0 +1,13 @@
+notebook01.Rmd
+data/movie_class.csv
+data/movies_top_20_2000_2019.csv
+notebook02.Rmd
+notebook03.Rmd
+notebook04.Rmd
+notebook05.Rmd
+data/sotu_document.csv.gz
+data/sotu_token.csv.gz
+topic.R
+data/scotus_cites_dh.csv
+data/scotus_dh.csv
+data/scotus_gr_all_dh.csv
---
+++ upload/topic.R
@@ -0,0 +1,86 @@
+library(jsonlite)
+library(tidyverse)
+
+topics_create_json <- function(topic_docs, topic_terms, meta, name_col, truncate = -1)
+{
+
+  tnames <- topic_terms %>%
+    arrange(topic, desc(beta)) %>%
+    group_by(topic) %>%
+    slice_head(n = 5) %>%
+    group_by(topic) %>%
+    summarize(name = paste(token, collapse = "; "))
+
+  topic_words <- topic_terms %>%
+    arrange(topic, desc(beta)) %>%
+    group_by(topic) %>%
+    slice_head(n = 100) %>%
+    mutate(weight = round(100 * exp(beta) / max(exp(beta)))) %>%
+    filter(weight > 0) %>%
+    ungroup()
+
+  topic_weights <- topic_docs %>%
+    group_by(topic) %>%
+    summarize(proportion = sum(prob)) %>%
+    mutate(proportion = proportion / sum(proportion) * 100)
+
+  top_docs <- topic_docs %>%
+    arrange(topic, desc(prob)) %>%
+    group_by(topic) %>%
+    filter(prob > 0) %>%
+    mutate(prob = round(prob * 100)) %>%
+    filter(prob > 0) %>%
+    ungroup()
+
+  name_lookup <- meta[,c("doc_id", name_col)]
+  names(name_lookup) <- c("doc_id", "title")
+  name_lookup <- group_by(name_lookup, doc_id, title) %>%
+    summarise(n = n()) %>%
+    ungroup()
+  
+  dset <- sort(unique(topic_docs$doc_id))
+  index <- match(dset, name_lookup$doc_id)
+  dset_title <-  name_lookup$title[index]
+  top_docs$id <- match(top_docs$doc_id, dset) - 1L
+
+  tset <- sort(unique(topic_terms$topic))
+  topics <- vector("list", length(tset))
+  for (j in seq_along(topics))
+  {
+    topics[[j]] <- list(
+      "short" = unbox(sprintf("Cluster %d", j)),
+      "long" = unbox(sprintf("Cluster %d: %s", j, tnames$name[tnames$topic == tset[j]])),
+      "proportion" = unbox(round(topic_weights$proportion[topic_weights$topic == tset[j]])),
+      "top_docs_ids" = top_docs$id[top_docs$topic == tset[j]],
+      "doc_perc" = top_docs$prob[top_docs$topic == tset[j]],
+      "top_word" = topic_words$token[topic_words$topic == tset[j]],
+      "word_wgt" = topic_words$weight[topic_words$topic == tset[j]]
+    )
+  }
+
+  top_topics <- topic_docs %>%
+    arrange(doc_id, desc(prob)) %>%
+    group_by(doc_id) %>%
+    filter(prob > 0) %>%
+    mutate(prob = round(prob * 100)) %>%
+    filter(prob > 0) %>%
+    ungroup()
+
+  top_topics$id <- match(top_topics$topic, tset) - 1L
+
+  docs <- vector("list", length(dset))
+  if (truncate > 0) { meta$text <- stringi::stri_sub(meta$text, 1, truncate) }
+  for (j in seq_along(docs))
+  {
+    docs[[j]] <- list(
+      "top_topics_ids" = top_topics$id[top_topics$doc_id == dset[j]],
+      "topic_weights" = top_topics$prob[top_topics$doc_id == dset[j]],
+      "title" = unbox(dset_title[j]),
+      "text" = meta$text[meta$doc_id == dset[j]],
+      "meta" = list()
+    )
+  }
+
+  output <- list(topics = topics, docs = docs)
+  return(output)
+}
